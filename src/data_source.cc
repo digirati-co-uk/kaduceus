5c630ab8ad0099e0a3b79deda03a94a772b4bf27
Gary Tierney <gary.tierney@fastmail.com>
2025-03-24 10:59:05 +0000

    Use fully-serviced mode for async IO

--- src/data_source.cc
+++ src/data_source.cc
@@ -7,41 +7,50 @@ namespace digirati::kaduceus {
 AsyncReaderCompressedSource::AsyncReaderCompressedSource(rust::Box<AsyncReader>& reader)
     : reader_(std::move(reader))
 {
+
+    std::thread([&]() {
+        enable(KDU_MEMBACKED_SOURCE_FULLY_SERVICED, NULL, 0, 0, -1.0, 0, 0);
+        size_t total_fetched_bytes = 0;
+        kdu_core::kdu_int32 term_code = 0;
+        service(total_fetched_bytes, term_code);
+    }).detach();
+
+    open(false);
 }
 
-// kdu_core::kdu_long AsyncReaderCompressedSource::fetch_data(kdu_core::kdu_long max_bytes,
-//     kdu_core::kdu_byte* buffer, bool blocking)
+kdu_core::kdu_long AsyncReaderCompressedSource::fetch_data(kdu_core::kdu_long max_bytes,
+    kdu_core::kdu_byte* buffer, bool blocking)
+{
+    if (!blocking) {
+        return -1;
+    }
+
+    const ssize_t read = reader_->read(rust::Slice(buffer, max_bytes));
+    if (read > SSIZE_MAX) {
+        throw std::runtime_error("reader returned too many bytes, this is an implementation error");
+    }
+
+    return static_cast<ssize_t>(read);
+}
+
+// bool AsyncReaderCompressedSource::seek(kdu_core::kdu_long offset)
 // {
-//     if (!blocking) {
-//         return -1;
-//     }
-
-//     const ssize_t read = reader_->read(rust::Slice(buffer, max_bytes));
-//     if (read > SSIZE_MAX) {
-//         throw std::runtime_error("reader returned too many bytes, this is an implementation error");
-//     }
-
-//     return static_cast<ssize_t>(read);
+//     reader_->seek(offset);
+//     pos = offset;
+//     return true;
 // }
 
-bool AsyncReaderCompressedSource::seek(kdu_core::kdu_long offset)
-{
-    reader_->seek(offset);
-    pos = offset;
-    return true;
-}
+// int AsyncReaderCompressedSource::read(kdu_core::kdu_byte* buf, int num_bytes)
+// {
+//     auto read = reader_->read(rust::Slice(buf, num_bytes));
+//     if (read > 0) {
+//         pos += (unsigned long)read;
+//     }
 
-int AsyncReaderCompressedSource::read(kdu_core::kdu_byte* buf, int num_bytes)
-{
-    auto read = reader_->read(rust::Slice(buf, num_bytes));
-    if (read > 0) {
-        pos += (unsigned long)read;
-    }
- 
-    return read;
-}
+//     return read;
+// }
 
-kdu_core::kdu_long AsyncReaderCompressedSource::get_pos() {
-    return pos;
-}
+// kdu_core::kdu_long AsyncReaderCompressedSource::get_pos() {
+//     return pos;
+// }
 }
--- src/data_source.h
+++ src/data_source.h
@@ -9,18 +9,18 @@
 namespace digirati::kaduceus {
 struct AsyncReader;
 
-class AsyncReaderCompressedSource final : public kdu_core::kdu_compressed_source {
+class AsyncReaderCompressedSource final : public kdu_core::kdu_membacked_compressed_source {
 public:
     explicit AsyncReaderCompressedSource(rust::Box<AsyncReader>& reader);
     virtual ~AsyncReaderCompressedSource() { close(); }
-    int get_capabilities() { return KDU_SOURCE_CAP_SEEKABLE | KDU_SOURCE_CAP_SEQUENTIAL; }
+    // int get_capabilities() { return KDU_SOURCE_CAP_SEEKABLE | KDU_SOURCE_CAP_SEQUENTIAL; }
 
-    int read(kdu_core::kdu_byte *buf, int num_bytes) override;
+    // int read(kdu_core::kdu_byte *buf, int num_bytes) override;
 protected:
 
-    // kdu_core::kdu_long fetch_data(kdu_core::kdu_long max_bytes, kdu_core::kdu_byte* buffer, bool blocking) override;
-    bool seek(kdu_core::kdu_long offset) override;
-    kdu_core::kdu_long get_pos() override;
+    kdu_core::kdu_long fetch_data(kdu_core::kdu_long max_bytes, kdu_core::kdu_byte* buffer, bool blocking) override;
+    // bool seek(kdu_core::kdu_long offset) override;
+    // kdu_core::kdu_long get_pos() override;
 private:
     unsigned long pos = 0;
     rust::Box<AsyncReader> reader_;
